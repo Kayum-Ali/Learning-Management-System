ce869b47c238f80d7a4ad3cd0692843bbc6f442f
Programmer-Manik <programmermanik1502083874@gmail.com>
2024-08-10 01:08:30 +0600

    manik hossain create herosection 1

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,28 @@
 module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
-      extend: {},
+      extend: {
+        screens: {
+          mq1325: {
+            raw: "screen and (max-width: 1325px)",
+          },
+          mq800: {
+            raw: "screen and (max-width: 800px)",
+          },
+          mq450: {
+            raw: "screen and (max-width: 450px)",
+          },
+        },
+      },
+      fontSize: {
+        xs: "12px",
+        base: "16px",
+        inherit: "inherit",
+      },
+     
+    },
+    corePlugins: {
+      preflight: false,
     },
     plugins: [],
   }
